6645c7c7ffc078332b161f0738f444b72d178be0
Taehun3536 <hg1282rnjs@gmail.com>
2026-05-05 17:33:07 +0900

    Design: 파비콘 아이콘 적용

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,9 @@ import Footer from "@/components/Footer";
 export const metadata = {
   title: "Engineer Portfolio",
   description: "품질과 성장을 지향하는 엔지니어 권태훈의 포트폴리오입니다.",
+  icons: {
+    icon: "/favicon.ico.png",
+  },
 };
 
 export default function RootLayout({ children }) {
